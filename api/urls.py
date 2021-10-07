4bce5d08349c0ed1e471f3308a043c8de797a331
adityasingh108 <adityasingh78085@gmail.com>
2021-10-07 14:36:32 +0530

    IMPLEMENT CUSTOM TOKEN

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path ,include
 from  api import views
 from rest_framework.routers import DefaultRouter
-from rest_framework.authtoken.views import obtain_auth_token
+from .CustomToken import CustomAuthToken
 
 router = DefaultRouter()
 
@@ -10,5 +10,6 @@ router.register('student',views.StudenetModelViewSet ,basename='Student')
 
 urlpatterns = [
     path('',include(router.urls)),
-    path('token/',obtain_auth_token),   
+    # path('token/',obtain_auth_token), 
+    path('token/',CustomAuthToken.as_view()),  
 ]
