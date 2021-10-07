dba228d34075776bd946df402ba7067b754de5b7
adityasingh108 <adityasingh78085@gmail.com>
2021-10-07 14:21:43 +0530

    USING TOKEN AUTHENTICATION

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path ,include
 from  api import views
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter()
 
@@ -8,5 +9,6 @@ router = DefaultRouter()
 router.register('student',views.StudenetModelViewSet ,basename='Student')
 
 urlpatterns = [
-    path('',include(router.urls))
+    path('',include(router.urls)),
+    path('token/',obtain_auth_token),   
 ]
--- api/views.py
+++ api/views.py
@@ -1,15 +1,18 @@
 from rest_framework import viewsets 
 from . models import Student
 from .serializers import StudentSerializers
-from rest_framework.authentication import BasicAuthentication ,SessionAuthentication
-from rest_framework.permissions import IsAuthenticated,AllowAny,IsAuthenticatedOrReadOnly,DjangoModelPermissions,DjangoModelPermissionsOrAnonReadOnly
-from .custumPermission import MyPermission
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
 # Create your views here.
 
 class StudenetModelViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializers 
-    authentication_classes = [SessionAuthentication]
-    # permission_classes = [IsAuthenticatedOrReadOnly ,IsAuthenticated,AllowAny,DjangoModelPermissionsOrAnonReadOnly]
-    # permission_classes = [DjangoModelPermissions]
-    permission_classes  = [MyPermission]
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+    
+    
+    
+ #http POST http://127.0.0.1:8000/api/token/ username="tester" password="passport123"
+ #to send post request to the url    
